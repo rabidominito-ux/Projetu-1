25a7d8ebc613b163e40471fddbad90be969e6113
rabidominito-ux <rabidominito@gmail.com>
2026-08-09 11:24:41 +0900

    Update models.py

--- models.py
+++ models.py
@@ -1,6 +1,6 @@
+import joblib
 import numpy as np
 import pandas as pd
-from sklearn.metrics import accuracy_score, classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.tree import DecisionTreeClassifier
@@ -23,4 +23,23 @@ def treinar_modelo(df, nota_cols, target_col):
     model = DecisionTreeClassifier(criterion="entropy", max_depth=5, random_state=42)
     model.fit(X_train, y_train)
     
+    # Salva automatikamente modelu ne'ebé foin treinu ba fail .pkl
+    salvar_modelo(model, le)
+    
     return model, le, X_train, X_test, y_train, y_test
+
+def carregar_modelo_pkl(filename="modelu_cfp.pkl"):
+    try:
+        dados_salvos = joblib.load(filename)
+        return dados_salvos
+    except Exception as e:
+        return None
+
+def salvar_modelo(model, le, filename="modelu_cfp.pkl"):
+    try:
+        # Rai hamutuk model no LabelEncoder iha formatu dictionary
+        joblib.dump({"model": model, "le": le}, filename)
+        return True
+    except Exception as e:
+        print(f"Erro atu salva modelu: {e}")
+        return False
